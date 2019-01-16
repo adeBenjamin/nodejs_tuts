b26abd29bb7d1946db543258e62fa9a7a51f479a
adeBenjamin <ab@adebenjamin.com>
2019-01-16 21:27:07 +0000

    added new cli arguments using the process.argv[] object

--- note_app/app.js
+++ note_app/app.js
@@ -8,3 +8,21 @@ const _ = require('lodash');
 
 //node modules I've written
 const notes = require('./notes.js');
+
+// argv is an arguments array of arguments passed in via cli. access the arguments via the process object and the argv method (arguments vector)
+// console.log(process.argv);
+
+var command = process.argv[2]; // [2] to access the 3rd (0, 1, 2) argument in the array
+console.log('Command: ', command);
+
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command not recognised');
+}
