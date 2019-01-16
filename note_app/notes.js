33a98a11a029e1a116b37c277047c55de02baa27
adeBenjamin <ab@adebenjamin.com>
2019-01-16 19:44:46 +0000

    added .gitignore file plus json files and other app changes

---
+++ note_app/notes.js
@@ -0,0 +1,23 @@
+console.log('Staring notes.js');
+
+/*
+ARROW FUNCTIONS SYNTAX
+function () {
+}......................becomes
+() => {
+}
+
+arrow fucntions dont bind the this keyword nor the arguments array
+*/
+
+// module.exports.addNote = () => {
+//   console.log('addNote');
+//   return 'New note';
+// };
+
+// write a function here that adds two numbers. call it in the app.js file passing two numbers and run app.js on the command line
+
+module.exports.addNum = (a, b) => {
+  console.log ('a ' + '+ ' + 'b');
+  return a + b;
+}
