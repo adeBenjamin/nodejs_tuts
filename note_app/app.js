db390b5ed1a6c32c1a5eb98e7ae8d95fcde396b5
adeBenjamin <ab@adebenjamin.com>
2019-01-17 13:38:46 +0000

    added new playground dir to test JSON code

--- note_app/app.js
+++ note_app/app.js
@@ -5,6 +5,7 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 //node modules I've written
 const notes = require('./notes.js');
@@ -12,17 +13,25 @@ const notes = require('./notes.js');
 // argv is an arguments array of arguments passed in via cli. access the arguments via the process object and the argv method (arguments vector)
 // console.log(process.argv);
 
-var command = process.argv[2]; // [2] to access the 3rd (0, 1, 2) argument in the array
+const argv = yargs.argv; // yargs.argv is where yargs library stores the arguments that the app runs with
+// var command = process.argv[2]; // [2] to access the 3rd (0, 1, 2) argument in the array using process.
+var command = argv._[0]; // using yargs to grab the first argument in the array
 console.log('Command: ', command);
+console.log('Process: ', process.argv); // without yargs
+console.log('Yargs: ', argv); // using yargs (easier and cleaner recognition of arguments)
 
 if (command === 'add') {
-  console.log('Adding new note');
+  // console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all notes');
+  // console.log('Listing all notes');
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('Reading note');
+  // console.log('Reading note');
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('Removing note');
+  // console.log('Removing note');
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognised');
 }
--- note_app/notes.js
+++ note_app/notes.js
@@ -9,11 +9,11 @@ function () {
 
 arrow fucntions dont bind the this keyword nor the arguments array
 */
-
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
-};
+//
+// module.exports.addNote = () => {
+//   console.log('addNote');
+//   return 'New note';
+// };
 
 // write a function here that adds two numbers. call it in the app.js file passing two numbers and run app.js on the command line
 //
@@ -21,3 +21,30 @@ module.exports.addNote = () => {
 //   console.log ('a ' + '+ ' + 'b');
 //   return a + b;
 // }
+
+var addNote = (title, body) => {
+  console.log('Adding note: ', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes: ');
+};
+
+var getNote = (title) => {
+  console.log('Getting note: ', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note: ', title);
+};
+
+// we have access to a variable inside all node files called module [use console.log(module) to see it] inside module property is an export object.
+// module.exports allows us to set properties that we can accss in app.js [in app.js you need require('./notes.js')]
+// call the functions above with module.exports.addNote, module.exports.getAll etc... but all in one go with an object like so:
+
+module.exports = {
+  addNote: addNote, // in ES6 theres a short cut for identical object property and value 'addNote' without columns will do
+  getAll,
+  getNote,
+  removeNote
+};
