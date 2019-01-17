f3085d1300c8b4bc262961c2f6bddea63c19afb5
adeBenjamin <ab@adebenjamin.com>
2019-01-17 17:21:29 +0000

    barbaershop just in case commit additional changes to json doc

--- note_app/playground/json.js
+++ note_app/playground/json.js
@@ -1,23 +1,25 @@
+//
+// // take a object and convert to a string
+// var obj = {
+//   name: 'King'
+// };
+// // new object stored in the variable obj
+// // call a JSON method to send it btw servers save it to a text file or other.
+//
+// // create a variable to store the result.
+// // JSON.stringify takes the object and coverts it to a string
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj); // just to check the result is indeed a string using the typeof operator
+// console.log(stringObj);
+//
+// //call node json.js in terminal for results
+//
+//
+// // take a string and convert to object
+// var personString = '{"name": "King", "age": 32}'; // DOUBLE QUOTES ONLY VALID IN JSON NOT JS
+// // using the opposite of JSON.stringify we'll use JSON.parse
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
 
-// take a object and convert to a string
-var obj = {
-  name: 'King'
-};
-// new object stored in the variable obj
-// call a JSON method to send it btw servers save it to a text file or other.
-
-// create a variable to store the result.
-// JSON.stringify takes the object and coverts it to a string
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj); // just to check the result is indeed a string using the typeof operator
-console.log(stringObj);
-
-//call node json.js in terminal for results
-
-
-// take a string and convert to object
-var personString = '{"name": "King", "age": 32}'; // DOUBLE QUOTES ONLY VALID IN JSON NOT JS
-// using the opposite of JSON.stringify we'll use JSON.parse
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+const fs = require('fs');
